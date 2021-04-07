36274595701693410b97440a26be267f215ffa9d
RANSHER <ransherraj@gmail.com>
2021-04-07 14:29:29 +0530

    Added form to capture input and all functionality of delete

---
+++ todolist/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+class TodoListForm(forms.Form):
+    text = forms.CharField(max_length=45,
+
+
+    widget = forms.TextInput(
+      attrs= {
+          'class':'form-control',
+          'placeholder': 'Enter todo e.g. Grocery Shopping',
+          'aria-label': 'Todo',
+          'area-describeby': 'add-btn'
+      }  
+    )
+    )
